0451d21c0ea946a00244826653fcb5f1070df82a
bcalisaya <bcalisaya@unsa.edu.pe>
2022-09-28 01:39:41 -0500

    first commit

---
+++ api/views.py
@@ -0,0 +1,8 @@
+from django.http import JsonResponse
+
+def api_home(request, *args, **kwargs):
+    return JsonResponse({
+        "message": "Hi there, this,is your Django API response!!"
+    })
+
+# Create your views here.
